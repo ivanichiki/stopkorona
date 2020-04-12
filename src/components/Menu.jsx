72ebf10315f4971205e0b42ac1ac3a611419c817
ivanichiki <62132967+ivanichiki@users.noreply.github.com>
2020-04-12 16:33:47 +0300

    mobile 0.18

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useSpring, animated } from 'react-spring'
 import { useDrag } from 'react-use-gesture'
 import '../styles/Menu.scss'
@@ -6,7 +6,8 @@ import group from '../svg/Group.png'
 import ok from '../svg/Ok.png'
 import lab from '../svg/Lab.png'
 import { NavLink } from 'react-router-dom'
-import Scrollbars from 'react-custom-scrollbars'
+
+
 
 const Block =(props)=> {
   return (
@@ -46,31 +47,59 @@ const Block =(props)=> {
 
 
 export const Menu = () => {
-  const [touch, settouch] = useState(false)
+
+const preventScrollRef = useRef(false)
+usePreventScroll(preventScrollRef)
+  function usePreventScroll(preventScrollRef) {
+    useEffect(() => {
+      const preventScrolling = e => {
+        if (preventScrollRef.current) {
+          e.preventDefault()
+        }
+      }
+  
+      document.addEventListener('touchmove', preventScrolling, {
+        passive: false,
+      })
+      return () => document.removeEventListener('touchmove', preventScrolling)
+    }, [])
+  }
+
   const [count, setcount] = useState(0)
-  const [props, set] = useSpring(() => ({ x: 0}))
-  const bind = useDrag(({ down, movement: [x] }) => {
+  const [props, set] = useSpring(() => ({ x: 0, y:0}))
+  const bind = useDrag(({ down, movement: [x, y] }) => {
    if (x>20||x<-20&x!=0) {
-     settouch(true)
-     document.body.style.overflow = "hidden";
+   
+    preventScrollRef.current = true
    }
    if (down==false) {
-    document.body.style.overflow = "auto";
-    settouch(false)
+    preventScrollRef.current = false
+  
    }
+//   if (down) {
+//     // Prevent scroll on touch screens while dragging:
+//     preventScrollRef.current = true
+//     }
+//  else {
+//     preventScrollRef.current = false
+//   }
     if (down==false&x<-50&x!=0&count!=200) {setcount(e=>e+100)}
     if (down==false&x>50&x!=0&count!=0) {setcount(e=>e-100)}
-    set({ x: down ? x : 0})
-
+    console.log(y)
+    set({ x: down&(y<40) ? x : 0,
+      immediate: down
+    })
+  
   })
 
   return (
   <div className={`menu_wrap_wrap`}>
-    <animated.div className={`Menu_wrapper`}  {...bind()} style={{...props,touchAction:`${touch?"none":"auto"}` ,right:`${count}vw`}}>
+    <animated.div ref={preventScrollRef} className={`Menu_wrapper`}  {...bind()} style={{...props ,right:`${count}vw`}}>
 
   <Block alink='https://стопкоронавирус.рф/ai/doc/4/attach/4.pdf' img={group} title='Памятка для пожилых людей' width='170px' tag='ЧТО ПРЕДПРИНЯТЬ?'/> 
      <Block  link='/what-is-done/council'  img={ok} title='Антикризисный план' width='135px' tag='ЧТО СДЕЛАНО?'/>
      <Block link='/'  img={lab} title='Где и как сдают анализы?' width='190px'tag='ВСЕ О КОРОНАВИРУСЕ'/>
+
 
     </animated.div>
     </div>
