9361b52f38cd3853730b0ac67650d4a14dcdb9d4
ivanichiki <62132967+ivanichiki@users.noreply.github.com>
2020-04-14 02:05:38 +0300

    Updates

---
+++ precache-manifest.70e78e31a01cc104f4de7bd6e1e67a4a.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d3f632aaa347ebaa86c04e7de7e9dd",
+    "url": "/stopkorona/index.html"
+  },
+  {
+    "revision": "85bd9acefb2d6b9a739d",
+    "url": "/stopkorona/static/css/main.d5c73b00.chunk.css"
+  },
+  {
+    "revision": "847841ddbe03255f817f",
+    "url": "/stopkorona/static/js/2.617b800e.chunk.js"
+  },
+  {
+    "revision": "e867553593e1bcee119717999d316408",
+    "url": "/stopkorona/static/js/2.617b800e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85bd9acefb2d6b9a739d",
+    "url": "/stopkorona/static/js/main.bd200b80.chunk.js"
+  },
+  {
+    "revision": "ebe0d00c82b8895a9d12",
+    "url": "/stopkorona/static/js/runtime-main.b585c0bb.js"
+  },
+  {
+    "revision": "7921b5862d0623241469a1762450648d",
+    "url": "/stopkorona/static/media/Cube.7921b586.svg"
+  },
+  {
+    "revision": "51cb785874daa30501a51e8d5c82b770",
+    "url": "/stopkorona/static/media/Group.51cb7858.png"
+  },
+  {
+    "revision": "724ba984649641f9908e97109f0dbd5b",
+    "url": "/stopkorona/static/media/Lab.724ba984.png"
+  },
+  {
+    "revision": "5f50dd995d2ba69c59c37a1d1e6c0eb6",
+    "url": "/stopkorona/static/media/Layer_8.5f50dd99.svg"
+  },
+  {
+    "revision": "1c2f25044f3ad552dadec3bddd6ceb12",
+    "url": "/stopkorona/static/media/Ok.1c2f2504.png"
+  },
+  {
+    "revision": "a38c9aede11ab84bb8fede667dba3da1",
+    "url": "/stopkorona/static/media/border.a38c9aed.png"
+  },
+  {
+    "revision": "fde5eac4c688f84d1893fab77da95e0e",
+    "url": "/stopkorona/static/media/call.fde5eac4.svg"
+  },
+  {
+    "revision": "e42ad7ee7f6c065ef85314f918b90b3b",
+    "url": "/stopkorona/static/media/close2.e42ad7ee.svg"
+  },
+  {
+    "revision": "929531249340525d00159bd00156df19",
+    "url": "/stopkorona/static/media/dostup.92953124.png"
+  },
+  {
+    "revision": "7e8a290bbbc613ca55d27b8f8174c632",
+    "url": "/stopkorona/static/media/download.7e8a290b.svg"
+  },
+  {
+    "revision": "5b78a45574d81be6c47773e5d8d31ca4",
+    "url": "/stopkorona/static/media/fly.5b78a455.svg"
+  },
+  {
+    "revision": "dd33aacaf880adf34b3d8a3568c73e19",
+    "url": "/stopkorona/static/media/globe.dd33aaca.svg"
+  },
+  {
+    "revision": "0bd5c1e5fbc9835f420a2877e0f91167",
+    "url": "/stopkorona/static/media/gov.0bd5c1e5.svg"
+  },
+  {
+    "revision": "f51daea7976de064d5fe53f8f63ec3a0",
+    "url": "/stopkorona/static/media/icq.f51daea7.svg"
+  },
+  {
+    "revision": "95bdfb7a5f9f7750fd5fd1399a50a2ed",
+    "url": "/stopkorona/static/media/instagram.95bdfb7a.svg"
+  },
+  {
+    "revision": "755dfe512582f9caa17b6ba6c1bec76f",
+    "url": "/stopkorona/static/media/kred_kanikuli.755dfe51.svg"
+  },
+  {
+    "revision": "ec41a22ebe55b1b53a5c8d8ad9db8d58",
+    "url": "/stopkorona/static/media/krediti_na_zarplatu.ec41a22e.svg"
+  },
+  {
+    "revision": "39abe24b4856c9b892c9cac0e5aa377a",
+    "url": "/stopkorona/static/media/line.39abe24b.svg"
+  },
+  {
+    "revision": "61f4145c5c98383d99e0b3093a24aeb0",
+    "url": "/stopkorona/static/media/load.61f4145c.svg"
+  },
+  {
+    "revision": "3b58c109b32af2f100aceb3c8ec4c944",
+    "url": "/stopkorona/static/media/logo.3b58c109.svg"
+  },
+  {
+    "revision": "375ff0f1de097ae30d34b53fa5b5a1f9",
+    "url": "/stopkorona/static/media/many.375ff0f1.svg"
+  },
+  {
+    "revision": "a1b0818537cb99a6f98b67cb6b6e1e9a",
+    "url": "/stopkorona/static/media/map.a1b08185.svg"
+  },
+  {
+    "revision": "a01f7f75b9749c2b8d3235b531bb7683",
+    "url": "/stopkorona/static/media/measure1.a01f7f75.svg"
+  },
+  {
+    "revision": "13f8ac451439a29ffc425a0cb5428be4",
+    "url": "/stopkorona/static/media/moratorii_na_proverki_biznesa.13f8ac45.svg"
+  },
+  {
+    "revision": "5a23b0c0f8fdef243132518b0e5339c6",
+    "url": "/stopkorona/static/media/myvmeste.5a23b0c0.png"
+  },
+  {
+    "revision": "e5cbfd59313465f1e09150f7ff05c6cc",
+    "url": "/stopkorona/static/media/mz.e5cbfd59.svg"
+  },
+  {
+    "revision": "fcfbcd6913d1ef87345f123dc421a8d7",
+    "url": "/stopkorona/static/media/nalogovie_kanikuli.fcfbcd69.svg"
+  },
+  {
+    "revision": "63601c254b04b758b976e500e46906e2",
+    "url": "/stopkorona/static/media/odnokashniki.63601c25.svg"
+  },
+  {
+    "revision": "8135be53e0d5be06670d76894c493126",
+    "url": "/stopkorona/static/media/otsrochka_arend_platexhei.8135be53.svg"
+  },
+  {
+    "revision": "495793455f9d2b30f9f0afe99a1b0585",
+    "url": "/stopkorona/static/media/pomosch_eksporteram.49579345.svg"
+  },
+  {
+    "revision": "09eeb55a0ce1db853c03ce58b04616a6",
+    "url": "/stopkorona/static/media/rpn.09eeb55a.svg"
+  },
+  {
+    "revision": "4916c2717cb3b559b8c1275f881b34c7",
+    "url": "/stopkorona/static/media/skorb.4916c271.svg"
+  },
+  {
+    "revision": "d22cd909065120dde2400099e39f2800",
+    "url": "/stopkorona/static/media/snimok.d22cd909.jpg"
+  },
+  {
+    "revision": "fa9ae479a95d9acde9897e58e6ccfe96",
+    "url": "/stopkorona/static/media/snixhenie_strah_vznosov_2_.fa9ae479.svg"
+  },
+  {
+    "revision": "4b16218aa73b039e75cc50066fe1d3e2",
+    "url": "/stopkorona/static/media/telega.4b16218a.svg"
+  },
+  {
+    "revision": "d7333227b8593971c2515803da4b6f64",
+    "url": "/stopkorona/static/media/vk.d7333227.svg"
+  },
+  {
+    "revision": "80b6998aec2552de2570766423021391",
+    "url": "/stopkorona/static/media/watsup.80b6998a.svg"
+  },
+  {
+    "revision": "bee9aab671772a73998a1a4eb1025b57",
+    "url": "/stopkorona/static/media/world.bee9aab6.svg"
+  },
+  {
+    "revision": "16cd78b0b0458628a7b1220e04143b4d",
+    "url": "/stopkorona/static/media/zig.16cd78b0.svg"
+  }
+]);
